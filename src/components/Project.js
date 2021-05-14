409c4d534d0fea02972447c50b380748592c82a8
Eric Heltemes <e.heltemes83@gmail.com>
2021-05-13 22:41:32 -0500

    updated img links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Project(props) {
+  console.log(process.env.PUBLIC_URL +props.img)
   return (
     <div className="project">
       <h3>{props.title}</h3>
       <p>{props.discription}</p>
       <div className="image">
-        <img src={props.img} alt={props.title}/>
+        <img src={process.env.PUBLIC_URL +props.img} alt={props.title}/>
       </div>
       <div className="links">
         <a href={props.deployedLink} target="_blank" rel="noreferrer">Deployed App</a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,13 @@ function Home() {
     <main>
       <div className="profile">
         <div className="photo">
-          <img src="/img/ERIC_HELTEMES_headshot.jpg" alt="eric_heltemes_headshot"/>
+          <img src={process.env.PUBLIC_URL + "/img/ERIC_HELTEMES_headshot.jpg"} alt="eric_heltemes_headshot"/>
         </div>
 
         <div id="about-me" className="about-me">
           <h2>About Me</h2>
           <p>I am a full-stack web developer who recently began the University of Minnesota's full-stack coding bootcamp. I still have a lot to learn, but am doing so at a rapid pace. </p>
-          <a href="/first-draft-resume.pdf" target="_blank" rel="noreferrer">Resume</a>
+          <a href={process.env.PUBLIC_URL + "/first-draft-resume.pdf"} target="_blank" rel="noreferrer">Resume</a>
         </div>
       </div>
 
