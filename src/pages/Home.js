f74791aa8cb7b0337d3567a76a716572a7ede8fb
Eric Heltemes <e.heltemes83@gmail.com>
2021-06-02 15:59:24 -0500

    added responsive elements to CSS and updated, About me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,9 @@ function Home() {
 
         <div id="about-me" className="about-me">
           <h2>About Me</h2>
-          <p>I am a full-stack web developer who recently began the University of Minnesota's full-stack coding bootcamp. I still have a lot to learn, but am doing so at a rapid pace. </p>
+          <p>
+          Full Stack Developer who blends systemic and creative approaches to solve problems. Earned certificate in web development from University of Minnesota where I honed my technical skills in node.js, databases, and react.js, and exercised my ability to learn quickly while still delivering quality products. Completing undergraduate degree in Music for Film TV & Games and working on many musical productions as a Music Director has given me experience in working with others to complete large projects both in leadership and collaborative positions. Driven to write efficient, organized, scaleable code that is easy for developers to modify while still considering the user experience Strong ability to plan ahead, work efficiently, communicate, and remain flexible.
+          </p>
           <a href={process.env.PUBLIC_URL + "/first-draft-resume.pdf"} target="_blank" rel="noreferrer">Resume</a>
         </div>
       </div>
